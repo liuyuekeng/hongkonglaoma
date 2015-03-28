b685d9acd3ee17bc5d05743db7fda00a6e81e28d
yueming <crazydogazrael@gmail.com>
2015-03-28 17:09:16 +0800

    add article list

--- conf/rjs_login_conf.js
+++ conf/rjs_login_conf.js
@@ -2,5 +2,4 @@
     baseUrl : '../statics/js',
     name: 'loginpage/login_main',
     out: '../statics/js/loginpage/index.js'
-
 })
--- jsx_src/article/article_list.js
+++ jsx_src/article/article_list.js
@@ -1,29 +1,56 @@
-var ArticleList = React.createClass({
-    render: function(){
-        var listItem = this.props.data.map(function(article){
+define(function(){
+    // 文章列表项
+    var ArticleListItem = React.createClass({
+        handleClick: function(ev){
+            location.pathname = '/articel/' + this.props.data._id;
+        },
+        render: function(){
+            var content = this.props.data.content;
+            // 缩略标识
+            var moreTag = content.indexOf('<!--more-->');
+            if(moreTag != -1){
+              content = content.slice(0,moreTag);
+            }
             return(
-                <ArticleListItem title={article.title} author={article.author}>
-                    {article.content}
-                </ArticleListItem>
-            );
-        });
-        return(
-            <div class="article-list">
-                {listItem}
-            </div>
-        );
-    }
-});
-
-var ArticleListItem = React.createClass({
-    render: function(){
-        return(
-            <div class="article-list-item">
-                <h2 class="article-list-item-title">{this.props.data}</h2>
-                <div class="article-list-item-excerpt">
-                    {this.props.children}
+                <div class="article-list-item">
+                    <h2 class="article-list-item-title" onClick{this.handleClick}=>{this.props.data.title}</h2>
+                    <div class="article-list-item-excerpt">
+                      {content}
+                    </div>
                 </div>
-            </div>
-        );
-    }
-});
+            );
+        }
+    });
+
+    // 分页
+    var PageTag = React.createClass({
+    });
+
+    // 文章列表
+    var ArticleList = React.createClass({
+        componentDidMount: function(){
+            this.state.List = [
+                {
+                    title: 'test1',
+                    content: 'test1`s content'
+                }
+            ];
+        },
+        getArticles: function(){
+        },
+        render: function(){
+            var listItem = this.props.data.map(function(article){
+                return(
+                    <ArticleListItem title={article.title} author={article.author}>
+                        {article.content}
+                    </ArticleListItem>
+                );
+            });
+            return(
+                <div class="article-list">
+                    {listItem}
+                </div>
+            );
+        }
+    });
+})

8160ca5b16299e24941af0021fc5d8ddad4eb8e3
yueming <crazydogazrael@gmail.com>
2015-03-28 15:34:38 +0800

    edit rjs_conf

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ echo '========================jsx start==============================='
 jsx jsx_src/ statics/js
 echo '========================jsx end================================='
 echo '========================rjs start==============================='
-node node_modules/requirejs/bin/r.js -o conf/rjs_conf.js optimize=none
+node node_modules/requirejs/bin/r.js -o conf/rjs_login_conf.js optimize=none
 echo '========================rjs end================================='

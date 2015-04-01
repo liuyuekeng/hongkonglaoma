c02f9709dae20f7afa3151dbbae2ceeab6a95a63
yueming <crazydogazrael@gmail.com>
2015-04-01 15:41:36 +0800

    add article api

--- actions/article_api.js
+++ actions/article_api.js
@@ -2,7 +2,27 @@ var express = require('express');
 var articleApi = express();
 var DB_articles = require('../modules/DB_articles.js');
 
+function initDbObj (req, res, next){
+    if (!req.db) {
+        res.status(500).json({
+            err : 'missing db in req'
+        });
+    } else {
+        req.db_article = DB_articles(req.db);
+        next();
+    }
+}
+
+articleApi.use(initDbObj);
+
 articleApi.get('/item', function (req, res) {
     res.send('article item');
+});
+
+articleApi.get('/list', function (req, res){
+    var page = req.query.page;      // 页码
+    var length = req.length.length; // 每页大小
+    
+    res.json(req.db_article.getArticlesList(page, length));
 });
 module.exports = articleApi;
